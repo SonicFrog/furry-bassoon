9d55e7dd87ad0b196dc071df97cf7770b04a154c
Ogier Bouvier <ars3nic@ars3nic.ch>
2018-03-26 16:56:04 +0200

    initial

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+mod map;
+
+use std::hash::Hash;
+use self::map::ConcurrentHashMap;
+
+fn main() {
+    let map: ConcurrentHashMap<u32, String> = ConcurrentHashMap::new(32, 32);
+
+    for i in 0..10 {
+        map.insert(&(i as u32), i.to_string());
+    }
+
+    for i in 0..10 {
+        println!("{:?}", map.get(&i).unwrap());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use map::ConcurrentHashMap;
+
+    const TEST_SET_SIZE: u32 = 4096;
+
+    #[test]
+    fn non_existent_key_is_none() {
+        let map: ConcurrentHashMap<String, u32> = ConcurrentHashMap::new(32, 32);
+        let s = String::from("non_existent");
+        assert_eq!(map.get(&s), None);
+    }
+
+    // #[bench]
+    // fn bench_access_32_32(b: &mut Bencher) {
+    //     let map: ConcurrentHashMap<String, u32> = ConcurrentHashMap::new(32, 32);
+
+    //     for i in 0..TEST_SET_SIZE {
+    //         map.insert(&i.to_string(), i);
+    //     }
+
+    //     b.iter(|| for i in 0..TEST_SET_SIZE { map.get(i.to_string()) })
+    // }
+
+    #[test]
+    fn existing_key_returns_some_value() {
+        let map: ConcurrentHashMap<String, u32> = ConcurrentHashMap::new(32, 32);
+        let k = String::from("valid key");
+        let v = 2000;
+
+        map.insert(&k, v);
+
+        assert_eq!(map.get(&k), Some(2000));
+    }
+
+    #[test]
+    fn remove_test() {
+        let map: ConcurrentHashMap<String, u32> = ConcurrentHashMap::new(32, 32);
+        let k = String::from("valid key");
+        let v = 2000;
+
+        map.insert(&k, v);
+        map.remove(&k);
+        assert_eq!(map.get(&k), None);
+    }
+}
